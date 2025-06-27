ef48b0c2e68bc2379a6366c9f3d2a8ad952547b0
JP Swart <juan-pierres@tfg.co.za>
2025-06-27 11:12:35 +0200

    Initial commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,22 @@
 module.exports = {
-  stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-a11y'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  webpackFinal: async (config) => {
+    // Add Babel loader for JS/JSX
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    });
+    return config;
+  },
 };
